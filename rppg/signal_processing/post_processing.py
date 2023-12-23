812376aaaa8c7c2213acf52425dfc7a58cb2ba65
CheremyP <pongajowc@gmail.com>
2023-12-23 22:56:39 +0100

    Added signal processing tools

--- rppg/signal_processing/post_processing.py
+++ rppg/signal_processing/post_processing.py
@@ -1,5 +1,14 @@
-#TODO: Averaging/Standardisation
-def detrend(input_signal, lambda_value = 100):
+import numpy as np
+import scipy.signal as signal
+from scipy import sparse
+
+def detrend(input_signal, lambda_value = 100) -> np.array():
+    if not isinstance(input_signal, np.ndarray):
+        raise ValueError("Input signal must be a NumPy array")
+    
+    if input_signal.ndim != 2:
+        raise ValueError("Input signal must be a 3D array with dimensions (RGB, frames)")
+    
     signal_length = input_signal.shape[0]
     # observation matrix
     H = np.identity(signal_length)
@@ -14,6 +23,12 @@ def detrend(input_signal, lambda_value = 100):
     return filtered_signal
 
 def cheby2_bandpass_filter(input_signal, lowcut, highcut, fs, order=4):
+    if not isinstance(input_signal, np.ndarray):
+        raise ValueError("Input signal must be a NumPy array")
+    
+    if input_signal.ndim != 2:
+        raise ValueError("Input signal must be a 3D array with dimensions (RGB, frames)")
+    
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -22,19 +37,41 @@ def cheby2_bandpass_filter(input_signal, lowcut, highcut, fs, order=4):
     return y
 
 def hilbert_filter(input_signal, lowcut, highcut, fs, order=4):
-    if input_signal != 3:
-        return ValueError
+    if not isinstance(input_signal, np.ndarray):
+        raise ValueError("Input signal must be a NumPy array")
     
+    if input_signal.ndim != 2:
+        raise ValueError("Input signal must be a 3D array with dimensions (RGB, frames)")
+    
+    HilberFiltered = np.empty_like(input_signal, dtype=np.float64)
+
     for channel in range(3):
-        analytic_signal = signal.hilbert(input_signal[:, channel])  # Apply hilbert to specific channel
-        amplitude_envelope = np.abs(analytic_signal)  # Derive envelope signal
-        HilberFiltered[:, channel] = BGRFiltered[:, channel] / amplitude_envelope
-    
+        analytic_signal = signal.hilbert(input_signal[:, channel]) 
+        amplitude_envelope = np.abs(analytic_signal)
+        HilberFiltered[:, channel] = input_signal[:, channel] / amplitude_envelope
+
     return HilberFiltered
 
 def normalisation(input_signal):
-    return NotImplementedError
+    if not isinstance(input_signal, np.ndarray):
+        raise ValueError("Input signal must be a NumPy array")
+    
+    if input_signal.ndim != 2:
+        raise ValueError("Input signal must be a 3D array with dimensions (RGB, frames)")
+
+    normalized_signal = (input_signal - np.min(input_signal, axis=-1, keepdims=True)) / (np.max(input_signal, axis=-1, keepdims=True) - 
+                                                                                         np.min(input_signal, axis=-1, keepdims=True))
+    return normalized_signal
 
 def standardisation(input_signal):
-    standardised_signal = input_signal
-    return standardised_signal
+    if not isinstance(input_signal, np.ndarray):
+        raise ValueError("Input signal must be a NumPy array")
+
+    if input_signal.ndim != 2:
+        raise ValueError("Input signal must be a 3D array with dimensions (RGB, frames)")
+    
+    mean = np.mean(input_signal, axis=-1, keepdims=True)
+    std = np.std(input_signal, axis=-1, keepdims=True)
+    standardized_signal = (input_signal - mean) / std
+
+    return standardized_signal
